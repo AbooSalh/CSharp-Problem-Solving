550a94707eaa3cb8f384b8937562e058e88ca5ad
Ahmed Saleh <a.m0507979@gmail.com>
2026-02-14 14:15:19 +0200

    Add MaxConsecutiveOnes and ShuffleTheArray problems
    Switched Program.cs to run MaxConsecutiveOnes instead of ConcatenationofArray. Added MaxConsecutiveOnes and ShuffleTheArray classes, both implementing IProblem, with methods for their respective array operations and sample inputs/outputs.

---
+++ Problems/MaxConsecutiveOnes.cs
@@ -0,0 +1,34 @@
+﻿namespace CSPS.Problems
+{
+    internal class MaxConsecutiveOnes : IProblem
+
+    {
+        public string Result
+        {
+            get
+            {
+                int[] nums = new int[] { 1, 1, 0, 1, 1, 1 };
+                return FindMaxConsecutiveOnes(nums).ToString();
+            }
+        }
+        public int FindMaxConsecutiveOnes(int[] nums)
+        {
+            int max = 0;
+            int current = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] == 1)
+                {
+                    current++;
+                }
+                else
+                {
+                    max = Math.Max(max, current);
+                    current = 0;
+                }
+
+            }
+            return Math.Max(max, current);
+        }
+    }
+}
---
+++ Problems/ShuffleTheArray.cs
@@ -0,0 +1,24 @@
+﻿namespace CSPS.Problems
+{
+    internal class ShuffleTheArray : IProblem
+    {
+        public string Result
+        {
+            get
+            {
+                int[] arr = [2, 5, 1, 3, 4, 7];
+                int n = 3;
+                int[] result = Shuffle(arr, n); return string.Join(", ", result);
+            }
+        }
+        public int[] Shuffle(int[] nums, int n)
+        {
+            int[] result = new int[nums.Length];
+            for (int i = 0; i < nums.Length; i++)
+            {
+                result[i] = i % 2 == 0 ? nums[i / 2] : nums[n + i / 2];
+            }
+            return result;
+        }
+    }
+}
