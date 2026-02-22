469d7bb5b230040619b3de18d2eebe92dd4e4cbe
Ahmed Saleh <a.m0507979@gmail.com>
2026-02-22 23:48:55 +0200

    Refactor FinalPrices method to use stack-based approach
    Refactored the `FinalPrices` method in `Final Prices With a Special Discount in a Shop.cs` to replace the inefficient nested loop implementation with a stack-based approach, improving performance by reducing redundant comparisons.
    
    Updated `Program.cs` to use the `Final_Prices_With_a_Special_Discount_in_a_Shop` problem instead of the `Daily_Temperatures` problem. Adjusted the `Main` method to reflect this change.
    
    Added a debug statement (`Console.WriteLine(" ss");`) in the `FinalPrices` method for testing purposes.

--- Problems/Final Prices With a Special Discount in a Shop.cs
+++ Problems/Final Prices With a Special Discount in a Shop.cs
@@ -11,44 +11,43 @@
                 return string.Join(", ", result);
             }
         }
-        public int[] FinalPrices(int[] prices)
-        {
-            int[] result = new int[prices.Length];
-            for (int i = 0; i < prices.Length; i++)
-            {
-                int discount = 0;
-                for (int j = i + 1; j < prices.Length; j++)
-                {
-                    if (prices[j] <= prices[i])
-                    {
-                        discount = prices[j];
-                        break;
-                    }
-                }
-                result[i] = prices[i] - discount;
-            }
-
-            return result;
-        }
         //public int[] FinalPrices(int[] prices)
         //{
-        //    Stack<int> stack = new();
-        //    int[] answer = new int[prices.Length];
+        //    int[] result = new int[prices.Length];
         //    for (int i = 0; i < prices.Length; i++)
         //    {
-        //        while (stack.Count > 0 && prices[stack.Peek()] >= prices[i])
+        //        int discount = 0;
+        //        for (int j = i + 1; j < prices.Length; j++)
         //        {
-        //            int index = stack.Pop();
-        //            answer[index] = prices[index] - prices[i];
+        //            if (prices[j] <= prices[i])
+        //            {
+        //                discount = prices[j];
+        //                break;
+        //            }
         //        }
-        //        stack.Push(i);
+        //        result[i] = prices[i] - discount;
         //    }
-        //    while (stack.Count > 0)
-        //    {
-        //        int index = stack.Pop();
-        //        answer[index] = prices[index];
-        //    }
-        //    return answer;
+
+        //    return result;
         //}
+        public int[] FinalPrices(int[] prices)
+        {
+            int n = prices.Length;
+            Stack<int> stack = new();
+            int[] result = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                while (stack.Count > 0 && prices[stack.Peek()] >= prices[i])
+                {
+
+                    int prevIndex = stack.Pop();
+                    result[prevIndex] = prices[prevIndex] - prices[i];
+                }
+                result[i] = prices[i]; // default price if no discount
+                stack.Push(i);
+            }
+            return result;
+
+        }
     }
 }
