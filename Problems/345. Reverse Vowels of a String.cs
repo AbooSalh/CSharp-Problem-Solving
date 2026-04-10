694d7fd917394da607299a7dfe559dd0113e8163
Ahmed Saleh <a.m0507979@gmail.com>
2026-04-10 09:09:05 +0200

    Add solution for 345. Reverse Vowels of a String
    Implement _345 with ReverseVowels method and update Program.cs to run this solution. Add placeholder class _605 for future "Can Place Flowers" problem.

---
+++ Problems/345. Reverse Vowels of a String.cs
@@ -0,0 +1,48 @@
+﻿namespace CSPS.Problems
+{
+    internal class _345 : IProblem
+    {
+        public string Result
+        {
+            get
+            {
+                _345 p = new _345();
+                return p.ReverseVowels("hello");
+            }
+        }
+        public string ReverseVowels(string s)
+        {
+            char[] vowels = ['a', 'e', 'i', 'o', 'u' , 'A', 'E', 'I', 'O', 'U'];
+            char[] charsOfString = s.ToCharArray();
+            Stack<char> chars = [];
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                char currentCharOfStr = charsOfString[i];
+                if (vowels.Contains(currentCharOfStr))
+                {
+                    chars.Push(currentCharOfStr);
+                }
+
+            }
+
+            _ = chars.Reverse();
+            
+            for (int i = 0; i < s.Length; i++)
+            {
+                char currentCharOfStr = charsOfString[i];                
+                if (vowels.Contains(currentCharOfStr))
+                {
+                    charsOfString[i] = chars.Pop();
+
+                }
+                else
+                {
+                    charsOfString[i] = currentCharOfStr;
+                }
+            }
+
+            return new string(charsOfString);
+        }
+    }
+}
