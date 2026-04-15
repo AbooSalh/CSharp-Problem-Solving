f8860aa35030f4133de08b3ab82dc0f2f7c8f821
Ahmed Saleh <a.m0507979@gmail.com>
2026-04-15 15:08:42 +0200

    Add solution for LeetCode 455: Assign Cookies problem
    Implemented class _455 in CSPS.Problems, solving the Assign Cookies problem using a two-pointer approach. Includes FindContentChildren method and a sample test in the Result property.

---
+++ Problems/455. Assign Cookies.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSPS.Problems
+{
+    internal class _455 : IProblem
+    {
+        public string Result
+        {
+            get
+            {
+                int[] g = [1,2,3];
+                int[] s = [1];
+                return FindContentChildren(g,s).ToString();
+            }
+        }
+        public int FindContentChildren(int[] g, int[] s)
+        {
+            Array.Sort(g);
+            Array.Sort(s);
+
+            // pointers
+            int i = 0;
+            int j = 0;
+
+            while (i< g.Length && j< s.Length)
+            {
+                if(s[j] >= g[i])
+                {
+                    i++;
+                }
+                j++;
+            }
+            return i;
+        }
+    }
+}
